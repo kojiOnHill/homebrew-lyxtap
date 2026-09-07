46b6e758c4ddf5f1dafba10688373d2ff5e88743
Koji Yokota <yokota@lyx.org>
2026-09-07 16:19:43 +0900

    Experimental implementation of mirros

---
+++ Casks/lyx-mirror.rb
@@ -0,0 +1,54 @@
+cask "lyx" do
+  version "2.5.2"
+  sha256 "329a4f45a04f2585eac7b44722f068fe24722481dcc8521dc284c02161fb2855"
+
+  #language "ja" do
+  #  url "https://ftp.yz.yamagata-u.ac.jp/pub/lyx/bin/#{version}/LyX-#{version.sub(/-RC/,"~RC")}+qt6-x86_64-arm64-cocoa.dmg"
+  #end
+  language "en", "ja" do
+    url "https://mirrors.ocf.berkeley.edu/lyx/bin/#{version}/LyX-#{version.sub(/-RC/,"~RC")}+qt6-x86_64-arm64-cocoa.dmg"
+  end
+  language "fr", default:true do
+    url "https://ftp.lip6.fr/pub/lyx/bin/#{version}/LyX-#{version.sub(/-RC/,"~RC")}+qt6-x86_64-arm64-cocoa.dmg"
+  end
+  name "LyX"
+  desc "GUI document processor based on the LaTeX typesetting system"
+  homepage "https://www.lyx.org/"
+
+  caveats do
+    unsigned_accessibility
+  end
+
+  depends_on cask: [ "mactex", "skim" ]
+
+  livecheck do
+    url "https://www.lyx.org/Download"
+    regex(/LyX[._-]v?(\d+(?:\.\d+)+)\+qt5/i)
+  end
+
+  # app "LyX.app", target: "LyX-#{version}.app"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/inkscape", target: "lyx-inkscape"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/lyx"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/lyxclient"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/lyxconvert"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/lyxeditor"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/maxima", target: "lyx-maxima"
+  # binary "#{appdir}/LyX-#{version}.app/Contents/MacOS/tex2lyx"
+
+  app "LyX.app"
+  binary "#{appdir}/LyX.app/Contents/MacOS/inkscape", target: "lyx-inkscape"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyx"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxclient"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxconvert"
+  binary "#{appdir}/LyX.app/Contents/MacOS/lyxeditor"
+  binary "#{appdir}/LyX.app/Contents/MacOS/maxima", target: "lyx-maxima"
+  binary "#{appdir}/LyX.app/Contents/MacOS/tex2lyx"
+
+  zap trash: [
+    "~/Library/Application Support/LyX-#{version.major_minor}",
+    "~/Library/Caches/com.apple.python/Applications/LyX-#{version}.app",
+    "~/Library/Preferences/org.lyx.LyX-#{version.major_minor}.plist",
+    "~/Library/Preferences/org.lyx.lyx.plist",
+    "~/Library/Saved Application State/org.lyx.lyx.savedState",
+  ]
+end
